0e78f4fae88ee59ba82b70f169103712ec64a3aa
Phillip Reid Claunch <54785925+philrclaunchky@users.noreply.github.com>
2020-08-28 02:17:51 -0400

    Update x-lossless-decoder.rb

--- x-lossless-decoder.rb
+++ x-lossless-decoder.rb
@@ -1,5 +1,5 @@
 cask "x-lossless-decoder" do
-  version "2019.10.04"
+  version "20191004"
   sha256 "6fedfede1f54d691b69f0d6dc5477062c57fe6c48b7f86624a47eb1b58a1e4ef"
 
   url "https://tmkk.undo.jp/xld/xld-#{version}.dmg"
