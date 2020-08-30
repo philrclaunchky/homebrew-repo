818ed21bf0ff8822c67dad26896d1f7171531359
Phillip Reid Claunch <54785925+philrclaunchky@users.noreply.github.com>
2020-08-30 01:40:34 -0400

    Update x-lossless-decoder.rb

--- x-lossless-decoder.rb
+++ x-lossless-decoder.rb
@@ -2,7 +2,7 @@ cask "x-lossless-decoder" do
   version "20191004"
   sha256 "6fedfede1f54d691b69f0d6dc5477062c57fe6c48b7f86624a47eb1b58a1e4ef"
 
-  url "https://tmkk.undo.jp/xld/xld-#{version}.dmg"
+  url "http://download1476.mediafire.com/0y0k00rgiyng/i9m0qnq2se3b6hs/xld-#{version}.dmg"
   name "X Lossless Decoder"
   homepage "https://tmkk.undo.jp/xld/index_e.html"
   
