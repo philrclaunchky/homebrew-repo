36abb41c26dc6a0258e5b093faa20a2a7e0c3c2c
Phillip Reid Claunch <54785925+philrclaunchky@users.noreply.github.com>
2020-09-09 16:59:52 -0400

    Update mkvtoolnix41.rb

--- mkvtoolnix41.rb
+++ mkvtoolnix41.rb
@@ -1,9 +1,9 @@
 cask "mkvtoolnix41" do
-  version "41.0.0"
+  version "41"
   sha256 "2eb34d57209f6dc4d8ec9809028affb0ce8a7edad8370b36abf8996edbb9ac86"
 
-  url "http://download1514.mediafire.com/rrhhrv10gzig/bi2iad8i1ffntfm/MKVToolNix-#{version}.dmg"
-  name "MKVToolNix"
+  url "http://download1514.mediafire.com/rrhhrv10gzig/bi2iad8i1ffntfm/MKVToolNix#{version}.dmg"
+  name "MKVToolNix41"
   homepage "https://mkvtoolnix.download/"
 
   conflicts_with formula: "mkvtoolnix"
